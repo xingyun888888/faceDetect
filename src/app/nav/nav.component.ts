14eb9d60dbabf802d7f71db09b972281bfc69f3e
smile <xingyun_888888@sina.com>
2017-12-20 17:36:53 +0800

    save

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -17,17 +17,18 @@ export class NavComponent implements OnInit {
 
   currentIndex:number = -1;
 
-  navList:any= [{name:"首页",children:[{name:"首页",routeName:"/detail"},{name:"首页",routeName:"/hello"},{name:"首页",routeName:"/userinfo"},{name:"首页"},{name:"首页"}]},{name:"工作台"},{name:"登记拟稿"},{name:"通知公告"},{name:"登记拟稿",children:[{name:'来问登记',routeName:"/demo"},{name:"发文拟稿"},{name:"一般申请"}]},{name:"通知公告"},{name:"工作台"},{name:"登记拟稿"},{name:"通知公告"},];
+  navList:any= [{name:"首页"},{name:"数据监控"},{name:"报表数据分析"},{name:"查询",children:[{name:'媒体查询',routeName:"/demo"},{name:"日志查询"},{name:"登录查询"},{name:"告警查询"}]},{name:"人脸库管理"},{name:"硬件管理",children:[{name:"摄像头信息",routeName:"/detail"},{name:"服务端信息",routeName:"/hello"},{name:"移动端硬件信息",routeName:"/userinfo"}]},{nme:"布控策略"},{name:"地图管理"},{name:"权限管理"},];
 
   showList(e,curIndex){
     this.currentIndex = curIndex;
   }
   ngOnInit() {
 
-     // this.http.get("/api/bioauth/test").subscribe((res)=>{
-     //     console.dir(res);
+    // this.http.get("/api/bioauth/test").subscribe((res)=>{
+    //     console.dir(res);
 
-     // })
+    // })
   }
 
 }
+
