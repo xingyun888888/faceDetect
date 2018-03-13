ab9c894ab43c1a0a98cb21b8149945093174c701
smile <xingyun_888888@sina.com>
2018-03-13 20:05:00 +0800

    save

--- src/app/directive/date-format.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DateFormatDirective } from './date-format.directive';
-
-describe('DateFormatDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DateFormatDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/directive/form-validate.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FormValidateDirective } from './form-validate.directive';
-
-describe('FormValidateDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FormValidateDirective();
-    expect(directive).toBeTruthy();
-  });
-});
